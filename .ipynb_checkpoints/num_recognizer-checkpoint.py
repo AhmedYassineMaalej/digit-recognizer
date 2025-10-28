0a183403e7377dbd812b174dd48190f0d3697cdb
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2025-10-28 02:50:23 +0100

    update project to tf 2.x

--- .ipynb_checkpoints/num_recognizer-checkpoint.py
+++ .ipynb_checkpoints/num_recognizer-checkpoint.py
@@ -1,13 +1,12 @@
-import sys
+import os, sys
 from math import sqrt
 
 import numpy as np
 import pygame
-
-import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-model = tf.keras.models.load_model("model.keras")
+model = load_model(r"C:\Users\MSI\Desktop\Code\Python\Number Recognizer\my_model.h5")
 
 
 pygame.init()
@@ -26,27 +25,30 @@ window_width = pixel_dim * 28
 
 
 class Pixel:
-    def __init__(self, row: int, col: int) -> None:
+    def __init__(self, row:int , col:int) -> None:
         self.row = row
         self.col = col
         self.activation = 0
         self.center_x, self.center_y = self.get_center()
+    
 
     def get_center(self) -> tuple[int, int]:
-        self.x = self.col * pixel_dim
-        self.y = self.row * pixel_dim
+        self.x = self.col * pixel_dim 
+        self.y = self.row * pixel_dim 
 
         center_y = self.y + pixel_dim // 2
         center_x = self.x + pixel_dim // 2
 
         return (center_x, center_y)
 
-    def get_dis(self, mouse_pos: tuple[int, int]):
+
+    def get_dis(self, mouse_pos:tuple[int, int]):
         mouse_x, mouse_y = mouse_pos
         x_dis = mouse_x - self.center_x
         y_dis = mouse_y - self.center_y
 
-        return sqrt(x_dis**2 + y_dis**2)
+        return sqrt(x_dis ** 2 + y_dis ** 2)
+
 
     def activate(self, mouse_pos):
         dis = self.get_dis(mouse_pos)
@@ -58,13 +60,11 @@ class Pixel:
 
 
 def f(x):
-    return -0.001 * (x**4) + 255
-
+    return  - 0.001 * (x ** 4) + 255
 
 def draw_pixels():
     for pixel in pixels:
         pixel.draw()
-
 
 def activate_pixels(mouse_pos):
     for pixel in pixels:
@@ -79,15 +79,17 @@ pixels = [image[i][j] for i in range(28) for j in range(28)]
 
 
 def main():
+    
     mouse_pressed = False
     while True:
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
                 pygame.quit()
 
+
             elif ev.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pressed = True
-
+            
             elif ev.type == pygame.MOUSEBUTTONUP:
                 mouse_pressed = False
 
@@ -98,17 +100,17 @@ def main():
             elif ev.type == pygame.MOUSEMOTION and mouse_pressed:
                 mouse_pos = pygame.mouse.get_pos()
                 activate_pixels(mouse_pos)
-
+        
         draw_pixels()
         pygame.display.update()
 
 
 data = main()
 
+
 data_arr = np.array(data)
 data_arr = np.array([data_arr])
 prediction = model.predict(data_arr)
-
 print("the number you wrote is: ", end="")
 print(np.argmax(prediction[0]))
 
